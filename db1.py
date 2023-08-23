fbfb72d7d5e206181e3cd342ddcc9956568e37c1
hongsp88 <hongsp88@nate.com>
2023-08-23 16:46:49 +0900

    0823 database

---
+++ db1.py
@@ -0,0 +1,38 @@
+# db1.py
+import sqlite3
+import Demo
+
+# memory에 저장하는 방법
+#con = sqlite3.connect(":memory:")
+# 파일에 저장
+con = sqlite3.connect("c:\\work\\test.db")
+cur = con.cursor()  # cursor 객체에서 구문실행
+
+cur.execute("create table if not exists PhoneBook " +
+    "(id integer primary key autoincrement, name text, phoneNum text);")
+
+cur.execute("insert into PhoneBook (name, phoneNum) values ('hong', '010-111');")
+
+# 입력
+name = "전우치"
+phoneNumber = "010-222"
+cur.execute("insert into PhoneBook (name, phoneNum) values (?, ?);", (name, phoneNumber))
+
+# 다중 레코드 입력
+datalist = (("박문수","010-123"), ("김길동","010-456"))
+cur.executemany("insert into PhoneBook (name, phoneNum) values (?, ?);", datalist)
+
+# 출력
+cur.execute("select * from PhoneBook;")
+# for row in cur:
+#     print(row)
+
+Demo.printf("fetchone()")
+print(cur.fetchone())
+Demo.printf("fetchmany(2)")
+print(cur.fetchmany(2))
+Demo.printf("fetchall()")
+cur.execute("select * from PhoneBook;")
+print(cur.fetchall())
+
+con.close()
